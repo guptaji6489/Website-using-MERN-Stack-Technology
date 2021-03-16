069f2e453bd9b12f4d2671d26e0bf080bc52a2a8
guptaji6489 <guptaji6489@gmail.com>
2021-03-16 19:20:42 +0530

    setup db connection

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+
+const PORT = process.env.PORT;
+
+
+const middleware = (req, res, next) => {
+    console.log("hello from middleware");
+    next();
+
+}
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+app.get('/about', middleware, (req, res) => {
+    res.send("hello from about world");
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`server running in ${PORT}`);
+});
